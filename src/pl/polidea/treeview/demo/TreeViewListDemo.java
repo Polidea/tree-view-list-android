b4df34a2dc775caafd87a20ae35e36e2543da5b8
Jarek Potiuk <jarek.potiuk@polidea.pl>
2011-03-06 15:00:23 +0100

    Now we can switch between different adapters.

--- src/pl/polidea/treeview/demo/TreeViewListDemo.java
+++ src/pl/polidea/treeview/demo/TreeViewListDemo.java
@@ -10,16 +10,92 @@ import pl.polidea.treeview.TreeStateManager;
 import pl.polidea.treeview.TreeViewAdapter;
 import pl.polidea.treeview.TreeViewList;
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class TreeViewListDemo extends Activity {
+    private final class FancyColouredVariousSizesAdapter extends TreeViewAdapter<Long> {
+        private FancyColouredVariousSizesAdapter(final Context context, final TreeStateManager<Long> treeStateManager,
+                final int numberOfLevels) {
+            super(context, treeStateManager, numberOfLevels);
+        }
+
+        @Override
+        public View getNewChildView(final TreeNodeInfo<Long> treeNodeInfo) {
+            final LinearLayout viewLayout = (LinearLayout) getLayoutInflater().inflate(R.layout.demo_list_item, null);
+            return updateView(viewLayout, treeNodeInfo);
+        }
+
+        @Override
+        public View updateView(final View view, final TreeNodeInfo<Long> treeNodeInfo) {
+            final LinearLayout viewLayout = (LinearLayout) view;
+            final TextView descriptionView = (TextView) viewLayout.findViewById(R.id.demo_list_item_description);
+            final TextView levelView = (TextView) viewLayout.findViewById(R.id.demo_list_item_level);
+            descriptionView.setText(getDescription(treeNodeInfo.getId()));
+            descriptionView.setTextSize(20 - 2 * treeNodeInfo.getLevel());
+            levelView.setText("" + treeNodeInfo.getLevel());
+            levelView.setTextSize(20 - 2 * treeNodeInfo.getLevel());
+            return viewLayout;
+        }
+
+        @Override
+        public Drawable getBackgroundDrawable(final TreeNodeInfo<Long> treeNodeInfo) {
+            switch (treeNodeInfo.getLevel()) {
+            case 0:
+                return new ColorDrawable(Color.WHITE);
+            case 1:
+                return new ColorDrawable(Color.GRAY);
+            case 2:
+                return new ColorDrawable(Color.YELLOW);
+            default:
+                return null;
+            }
+        }
+
+        @Override
+        public long getItemId(final int position) {
+            return getTreeId(position);
+        }
+    }
+
+    private final class SimpleStandardAdapter extends TreeViewAdapter<Long> {
+        private SimpleStandardAdapter(final Context context, final TreeStateManager<Long> treeStateManager,
+                final int numberOfLevels) {
+            super(context, treeStateManager, numberOfLevels);
+        }
+
+        @Override
+        public View getNewChildView(final TreeNodeInfo<Long> treeNodeInfo) {
+            final LinearLayout viewLayout = (LinearLayout) getLayoutInflater().inflate(R.layout.demo_list_item, null);
+            return updateView(viewLayout, treeNodeInfo);
+        }
+
+        @Override
+        public View updateView(final View view, final TreeNodeInfo<Long> treeNodeInfo) {
+            final LinearLayout viewLayout = (LinearLayout) view;
+            final TextView descriptionView = (TextView) viewLayout.findViewById(R.id.demo_list_item_description);
+            final TextView levelView = (TextView) viewLayout.findViewById(R.id.demo_list_item_level);
+            descriptionView.setText(getDescription(treeNodeInfo.getId()));
+            levelView.setText("" + treeNodeInfo.getLevel());
+            return viewLayout;
+        }
+
+        @Override
+        public long getItemId(final int position) {
+            return getTreeId(position);
+        }
+    }
+
     private static final String TAG = TreeViewListDemo.class.getSimpleName();
     private TreeViewList<Long> treeView;
 
@@ -28,7 +104,8 @@ public class TreeViewListDemo extends Activity {
     private static final int levelNumber = 4;
     private final TreeStateManager<Long> manager = new InMemoryTreeStateManager<Long>();
     private final TreeBuilder<Long> treeBuilder = new TreeBuilder<Long>(manager);
-    private TreeViewAdapter<Long> adapter;
+    private FancyColouredVariousSizesAdapter fancyAdapter;
+    private SimpleStandardAdapter simpleAdapter;
 
     private String getDescription(final long id) {
         final Integer[] hierarchy = manager.getHierarchyDescription(id);
@@ -46,45 +123,30 @@ public class TreeViewListDemo extends Activity {
         }
         Log.d(TAG, manager.toString());
         treeView = (TreeViewList<Long>) findViewById(R.id.mainTreeView);
-        adapter = new TreeViewAdapter<Long>(this, manager, levelNumber) {
-            @Override
-            public View getNewChildView(final TreeNodeInfo<Long> treeNodeInfo) {
-                final LinearLayout viewLayout = (LinearLayout) getLayoutInflater().inflate(R.layout.demo_list_item,
-                        null);
-                return updateView(viewLayout, treeNodeInfo);
-            }
+        fancyAdapter = new FancyColouredVariousSizesAdapter(this, manager, levelNumber);
+        simpleAdapter = new SimpleStandardAdapter(this, manager, levelNumber);
+        treeView.setTreeViewAdapter(simpleAdapter);
+    }
 
-            @Override
-            public View updateView(final View view, final TreeNodeInfo<Long> treeNodeInfo) {
-                final LinearLayout viewLayout = (LinearLayout) view;
-                final TextView descriptionView = (TextView) viewLayout.findViewById(R.id.demo_list_item_description);
-                final TextView levelView = (TextView) viewLayout.findViewById(R.id.demo_list_item_level);
-                descriptionView.setText(getDescription(treeNodeInfo.getId()));
-                descriptionView.setTextSize(20 - 2 * treeNodeInfo.getLevel());
-                levelView.setText("" + treeNodeInfo.getLevel());
-                levelView.setTextSize(20 - 2 * treeNodeInfo.getLevel());
-                return viewLayout;
-            }
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        final MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.adapter_menu, menu);
+        return true;
+    }
 
-            @Override
-            public Drawable getBackgroundDrawable(final TreeNodeInfo<Long> treeNodeInfo) {
-                switch (treeNodeInfo.getLevel()) {
-                case 0:
-                    return new ColorDrawable(Color.WHITE);
-                case 1:
-                    return new ColorDrawable(Color.GRAY);
-                case 2:
-                    return new ColorDrawable(Color.YELLOW);
-                default:
-                    return null;
-                }
-            };
-
-            @Override
-            public long getItemId(final int position) {
-                return getTreeId(position);
-            }
-        };
-        treeView.setTreeViewAdapter(adapter);
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        switch (item.getItemId()) {
+        case R.id.simple_menu_item:
+            treeView.setTreeViewAdapter(simpleAdapter);
+            break;
+        case R.id.fancy_menu_item:
+            treeView.setTreeViewAdapter(fancyAdapter);
+            break;
+        default:
+            return false;
+        }
+        return true;
     }
 }
