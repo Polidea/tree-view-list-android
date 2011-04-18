88afbcff93d19f6a34be8cc606d8e7590df1fe26
Jarek Potiuk <jarek.potiuk@polidea.pl>
2011-04-19 01:43:21 +0200

    Updated to latest templates

--- Android-OTA/CreateOTA.py
+++ Android-OTA/CreateOTA.py
@@ -14,10 +14,10 @@ logging.basicConfig(level = logging.DEBUG)
 
 parser = OptionParser()
 parser.add_option('-n', '--app-name', action='store', dest='appName', help='aplication name')
-parser.add_option('-u', '--release-url', action='store', dest='releaseUrl', help='URL of directory where OTA files are externally visible')
+parser.add_option('-u', '--release-urls', action='store', dest='releaseUrls', help='URLs of download files - as coma separated list of entrires')
 parser.add_option('-d', '--destination-directory', action='store', dest='otaAppDir', help='Directory where OTA files are created')
 parser.add_option('-v', '--version', action='store', dest='version', help='Version of the application')
-parser.add_option('-r', '--release', action='store', dest='release', help='Release of the application')
+parser.add_option('-r', '--releases', action='store', dest='releases', help='Release names of the application')
 parser.add_option('-R', '--release-notes', action='store', dest='releaseNotes', help='Release notes of the application (in txt2tags format)')
 parser.add_option('-D', '--description', action='store', dest='description', help='Description of the application (in txt2tags format)')
 
@@ -25,24 +25,24 @@ parser.add_option('-D', '--description', action='store', dest='description', hel
 
 if options.appName == None:
     parser.error("Please specify the appName.")
-elif options.releaseUrl == None:
-    parser.error("Please specify releaseUrl")
+elif options.releaseUrls == None:
+    parser.error("Please specify releaseUrls")
 elif options.otaAppDir == None:
     parser.error("Please specify destination directory")
 elif options.version == None:
     parser.error("Please specify version")
-elif options.release == None:
-    parser.error("Please specify release")
+elif options.releases == None:
+    parser.error("Please specify releases")
 elif options.releaseNotes == None:
     parser.error("Please specify releaseNotes")
 elif options.description == None:
     parser.error("Please specify description")
 
 appName = options.appName
-releaseUrl = options.releaseUrl
+releaseUrls = options.releaseUrls
 otaAppDir = options.otaAppDir
 version = options.version
-release = options.release
+releases = options.releases
 releaseNotes = options.releaseNotes
 description = options.description
 
@@ -65,7 +65,7 @@ def createOTApackage():
 
     releaseNotesContent = open(releaseNotes).read()
     descriptionContent = open(description).read()
-    indexGenerator = IndexGenerator(appName, releaseUrl, releaseNotesContent, descriptionContent, version, release)
+    indexGenerator = IndexGenerator(appName, releaseUrls, releaseNotesContent, descriptionContent, version, releases)
     index = indexGenerator.get();
     tempIndexFile = tempfile.TemporaryFile()
     tempIndexFile.write(index)
